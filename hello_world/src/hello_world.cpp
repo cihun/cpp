22f5851ce16b6d2878fae47c992ea2b7cbc79367
L-鲁璞亮 <puliang.lu@fibocom.com>
2022-09-19 16:37:58 +0800

    111

--- hello_world/src/hello_world.cpp
+++ hello_world/src/hello_world.cpp
@@ -1,8 +1,19 @@
 #include "hello_world.h"
-
+#include <stdio.h>
+#include <stdlib.h>
+using namespace std;
 void print(int n){
+    n = 1;
     int i;
     for(i = 0;i < n;i++){
-        std::cout << "Hello World!" << std::endl;
+        cout << "Hello World!" << endl;
     }
+    char a[10], b[10];
+    scanf("%s%s",a,b);
+    gets(a,10)
+    char x[] = "xxxx";
+    cout << x << endl;
+    char y[20] = "xxxcccc";
+    cout << y;
+    
 }
