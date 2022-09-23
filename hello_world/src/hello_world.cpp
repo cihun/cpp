6dd44b602f0443c0411a407e4378b6ae4f7240ab
L-鲁璞亮 <puliang.lu@fibocom.com>
2022-09-23 18:40:09 +0800

    C++里面进行了一些测试，增加了一些C++的笔记。

--- hello_world/src/hello_world.cpp
+++ hello_world/src/hello_world.cpp
@@ -9,10 +9,6 @@ void print(int n){
         cout << "Hello World!" << endl;
     }
     char a[10], b[10];
-    scanf("%s%s",a,b);
-    gets(a,10)
-    char x[] = "xxxx";
-    cout << x << endl;
     char y[20] = "xxxcccc";
     cout << y;
     
--- hello_world/src/main.cpp
+++ hello_world/src/main.cpp
@@ -2,16 +2,21 @@
 #include<fstream>
 #include "hello_world.h"
 #include "string1.h"
+#include <vector>
+#include <string>
+
 using namespace std;
 int main(){
     print(10);
-
-    ofstream f;
-    f.open("./xxx",ios::out);
-    f << "asdfasdf" << endl;
+    vector <string> x;
+    string y = "xxx";
+    x.push_back("aaaaa");
+    x.push_back(y);
+    y += "zzz";
+    x.push_back(y);
+    cout << x[0] << x[1] << x[2] << endl;
     String s;
     cin >> s;
     cout << s << endl;
-    f.close();
     return 0;
 }
